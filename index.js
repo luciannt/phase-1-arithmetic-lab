4e92540bf3e4a64b8d7b15026ab8b6438796bf85
Lucianna Tittle <lucianna.roset@gmail.com>
2022-08-08 22:15:34 -0500

    commit tree 9fcaca8aae955cf5d25b527680f4ec87d1e1a402

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Test 1
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+//Test 2
+const random = Math.floor(Math.random() * 100);
+
+//Test 3
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+//Test 4
+let max = Math.max(4, 8, 12, 16, 20);
